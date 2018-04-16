cce630c867727a9279229f96b2b3cfdcb4cce87c
Michel Davalos <michel.davalos@academicos.udg.mx>
2018-04-16 14:02:32 -0500

    agregada la pantalla para los cultivos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {CultivosPage} from "../cultivos/cultivos";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  cultivosPage = CultivosPage;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  cambiarCultivos() {
+    this.navCtrl.push(this.cultivosPage);
   }
 
 }
