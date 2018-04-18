2fcd736c46c57c840e81824c76059cb91d7ae943
Michel Davalos <michel.davalos@academicos.udg.mx>
2018-04-18 14:30:33 -0500

    agregado el evento click del boton

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HomePage} from "../home/home";
 
 /**
  * Generated class for the LoginPage page.
@@ -15,11 +16,24 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
+  usuario:string = "";
+  password:string = "";
+  home = HomePage;
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  clickBoton() {
+    console.log(this.usuario);
+    console.log(this.password);
+
+    this.navCtrl.setRoot(this.home);
+
   }
 
 }
